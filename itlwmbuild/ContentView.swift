27e5d141d1128ef7736517266f1245879ff37b25
lunjielee <q531303226@gmail.com>
2020-07-26 13:25:29 -0400

    Initial Commit

---
+++ itlwmbuild/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  itlwmbuild
+//
+//  Created by Lunjie Li on 2020-07-26.
+//  Copyright © 2020 Lunjie Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
